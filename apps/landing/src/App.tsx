b45c391d660ef87b497f4a649c417bd78f7ab32d
Bella MENG <bellamengzihan@gmail.com>
2026-02-10 22:52:01 +0000

    add landing page

---
+++ apps/landing/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+    return (
+        <div className="flex min-h-screen flex-col bg-pink-50">
+            <main className="flex flex-1 items-center justify-center">
+                <h1 className="text-2xl" style={{ color: "deeppink" }}>
+                    Hello world
+                </h1>
+            </main>
+            <footer className="py-6 text-center text-xs text-gray-500">
+                &copy; 2026 Pixattica
+            </footer>
+        </div>
+    );
+}
+
+export default App;
