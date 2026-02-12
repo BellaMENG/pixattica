7ea21351927a4efa3a80196f1cd24f62fa54d4d6
Bella MENG <bellamengzihan@gmail.com>
2026-02-12 21:53:34 +0000

    use static favicon for landing page

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { animateFavicon } from "@pixattica/ui";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -9,5 +8,3 @@ createRoot(document.getElementById("root")!).render(
         <App />
     </StrictMode>,
 );
-
-animateFavicon();
